50975c8b45710e18ac8ecc096b0d659cb31a60ce
AssuranceGit <50028443+AssuranceGit@users.noreply.github.com>
2019-04-26 11:02:24 -0700

    \s required since regular space is ignored in x-mode

--- lib/street_address.rb
+++ lib/street_address.rb
@@ -663,7 +663,7 @@ module StreetAddress
     self.address_regexp = /
       \A
       [^\w\x23]*    # skip non-word chars except # (eg unit)
-      #{number_regexp} \W*
+      #{number_regexp}\s\W*
       (?:#{fraction_regexp}\W*)?
       #{street_regexp}\W+
       (?:#{unit_regexp}\W+)?
